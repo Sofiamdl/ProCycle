080c36823a4d39888543aed2118670766c73c999
Sofia Melo <sofianovica@hotmail.com>
2023-05-23 15:31:41 -0300

    main: create base of project

---
+++ ProCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProCycle
+//
+//  Created by sml on 23/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        super.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
