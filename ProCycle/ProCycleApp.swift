aeb425b0e9ae745e82fd89d05f55bbd737eef333
Sofia Melo <sofianovica@hotmail.com>
2023-05-24 14:42:32 -0300

    develop: add swiftui project

---
+++ ProCycle/ProCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProCycleApp.swift
+//  ProCycle
+//
+//  Created by sml on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ProCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
