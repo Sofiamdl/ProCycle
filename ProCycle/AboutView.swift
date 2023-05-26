51496ec092f24e6ef604a075177aea0035de0700
Joana Lima <joanalima@MacBook-Pro-de-Joana.local>
2023-05-26 17:26:31 -0300

    Created the three onboarding screens

---
+++ ProCycle/AboutView.swift
@@ -0,0 +1,52 @@
+//
+//  AboutView.swift
+//  ProCycle
+//
+//  Created by Joana Lima on 26/05/23.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var window = NSScreen.main?.visibleFrame
+    var body: some View {
+        ZStack{
+            Color.purple
+                .ignoresSafeArea()
+            VStack{
+                Text("O que é o ProCycle?")
+                    .font(.system(size: window!.width / 50, weight: .semibold, design: .rounded))
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                
+                
+                Text("Pensando nisso, criamos o ProCycle, um aplicativo de acompanhamento menstrual um pouco diferente. Nele, além de saber as fases do seu ciclo, também é possível obter sugestões de como organizar melhor a sua rotina tendo como base o seu ciclo menstrual.")
+                    .font(.system(size: window!.width / 80, weight: .semibold, design: .rounded))
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: window!.width / 2, height: window!.height / 4)
+                
+                Text("Fornecemos informações detalhadas sobre cada uma das quatro fases do seu ciclo (Menstruação, Fase Folicular, Período Fértil e Fase Lútea) com dicas e recomendações de como realizar atividades dos mais variados tipos, desde momento mais propício pra reuniões até tipos de exercícios físicos, além de ser possível marcar e categorizar atividades com integração ao Calendário da Apple.")
+                    .font(.system(size: window!.width / 80, weight: .semibold, design: .rounded))
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: window!.width / 2, height: window!.height / 4)
+                
+                NavigationLink(destination: AboutView().navigationBarBackButtonHidden(true)) {
+                    
+                    Text("Continuar")
+                        .padding(25)
+                        .background(.white)
+                        .cornerRadius(40)
+                }
+                
+            }
+        }
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
---
+++ ProCycle/WhyView.swift
@@ -0,0 +1,20 @@
+//
+//  WhyView.swift
+//  ProCycle
+//
+//  Created by Joana Lima on 26/05/23.
+//
+
+import SwiftUI
+
+struct WhyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WhyView_Previews: PreviewProvider {
+    static var previews: some View {
+        WhyView()
+    }
+}
