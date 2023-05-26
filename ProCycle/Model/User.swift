361f6bc5cb5120b45c6f387a8c761dee60b3936d
mvitoriapereirac <mvitoriapereirac@gmail.com>
2023-05-26 16:29:15 -0300

    update

---
+++ ProCycle/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ProCycle
+//
+//  Created by mvitoriapereirac on 26/05/23.
+//
+
+import Foundation
+
+struct User {
+    let lastPeriodDay: Int
+    let cycleDuration: Int
+    let periodDuration: Int
+    let folicularDuration: Int
+    let fertileDuration: Int
+    let luthealDuration: Int 
+}
