ab7829d32a2abd8517e59094aedd335dc0b57592
joaopbarret <jpbol@cin.ufpe.br>
2023-05-29 17:44:19 -0300

    feature(set_calendar): create Event function

--- ProCycle/View/ContentView.swift
+++ ProCycle/View/ContentView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var calendar = CalendarViewModel()
-
+    @State var daysAfterMenstruation = "15"
+    @State var averageDuration = "5"
+    @State var averageCycle = "28"
+    @State var startDate = Date.now
+    @State var endDate = Date()
     var body: some View {
         VStack {
             Slider()
+            VStack {
+                TextField ("dias que fazem da ultima", text: $daysAfterMenstruation)
+                    .padding()
+                TextField ("duração media", text: $averageDuration)
+                    .padding()
+                TextField ("duraçao do ciclo", text: $averageCycle)
+                    .padding()
+            }
+            Button("criar evento", action: {
+                startDate -= Double(daysAfterMenstruation)!*24*3600
+                endDate = startDate + Double(averageDuration)!*24*3600
+                calendar.createEvent(title: "", startDate: startDate, endDate:endDate)
+            })
         }
         .environmentObject(calendar)
         .padding()
